4548de2dbe5eb782c9e53168b7fbe75b2b996974
asmaniac <adrum0070@launchpadphilly.org>
2025-10-09 16:03:03 -0400

    adding more code

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React, { useState } from 'react';
 import { searchMovies } from './api/searchMovies';
 
 function App() {
-  const [query, setQuery] = useState(''); // Thiss will store what the user types in
-  const {results, setResults} = useState({}) // Stores the results/movies that came up 
+  const [query, setQuery] = useState(''); // This stores what the user types in
+  const [results, setResults] = useState([]); // Stores the results/movies that came up
 
-// search function part
-  const handleSearch = async () => {
+  // search function part, handleSearch calls my searchMovies API
+  const handleSearch = async () => { 
+    if (!query) return; // this returns nothing if the search is just empty
 
+    try { // trying to handle the errors
+      const data = await searchMovies(query);
+      if (data && data.Search) { // if there are results, set them
+        setResults(data.Search);
+      } else {
+        setResults([]);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setResults([]);
+    }
+  }; 
 
-  }
-``
-}
+} 
